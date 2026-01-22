ee09d3e5d1191ea83934390d80d42b9fd68b6c30
Ryan Smith <73793381+BestSpark687090@users.noreply.github.com>
2026-01-22 19:25:06 +0000

    h

--- src/index.js
+++ src/index.js
@@ -14,25 +14,33 @@ export default {
 
 	// },
 	async fetch(request, env, ctx) {
-		// write a key-value pair
-		await env.KV.put('KEY', 'VALUE');
+		const url = new URL(request.url);
+		if (url.pathname.startsWith('/api/')) {
+			// TODO: Add your custom /api/* logic here.
+			return new Response('Ok');
+		}
+		// Passes the incoming request through to the assets binding.
+		// No asset matched this request, so this will evaluate `not_found_handling` behavior.
+		return env.ASSETS.fetch(request);
+		// // write a key-value pair
+		// await env.KV.put('KEY', 'VALUE');
 
-		// read a key-value pair
-		const value = await env.KV.get('KEY');
+		// // read a key-value pair
+		// const value = await env.KV.get('KEY');
 
-		// list all key-value pairs
-		const allKeys = await env.KV.list();
+		// // list all key-value pairs
+		// const allKeys = await env.KV.list();
 
-		// delete a key-value pair
-		await env.KV.delete('KEY');
+		// // delete a key-value pair
+		// await env.KV.delete('KEY');
 
-		// return a Workers response
-		return new Response(
-			JSON.stringify({
-				value: value,
-				allKeys: allKeys,
-			})
-		);
+		// // return a Workers response
+		// return new Response(
+		// 	JSON.stringify({
+		// 		value: value,
+		// 		allKeys: allKeys,
+		// 	})
+		// );
 	},
 };
 /*export default {
